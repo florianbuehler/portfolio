7587d381d1849ed3ea0096cdd7d0ac3fe82054ca
Florian Bühler <florian-buehler@outlook.com>
2022-08-24 20:44:42 +0200

    Improve hero content.

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -15,7 +15,7 @@ const StyledHeroSection = styled.section`
     padding-bottom: 0;
   }
 
-  h1 {ê
+  h1 {
     margin: 0 0 20px 2px;
     color: ${({ theme }) => theme.colors.primary};
     font-family: ${({ theme }) => theme.typography.fontMono};
@@ -36,7 +36,7 @@ const StyledHeroSection = styled.section`
 
   p {
     margin: 20px 0 0;
-    max-width: 540px;
+    max-width: 700px;
   }
 
   .action-link {
@@ -65,10 +65,15 @@ const HeroSection: React.FC = () => {
   items.push(<h3 className="big-heading">I build things for the web.</h3>);
   items.push(
     <p>
-      I’m a software engineer specializing in building (and occasionally designing) exceptional
-      digital experiences. Currently, I’m focused on building accessible, human-centered products at{' '}
-      <a href="https://google.com/" target="_blank" rel="noopener noreferrer">
-        Company
+      I’m a full stack software engineer specializing in building (and occasionally designing)
+      exceptional digital experiences. Currently, I’m focused on building reliable and highly secure
+      applications at{' '}
+      <a
+        href="https://www.boerse-stuttgart.de/en/about-us/company-structure/our-companies/blocknox-gmbh/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        blocknox
       </a>
       .
     </p>
